3f1b33c2cb3278ad5bd70b6afe944ef9f9472987
DeeKeilholz <deekeilholz@gmail.com>
2016-11-07 23:56:49 +0000

    update index file

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var GITHUB_USER = "DeeKeilholz";
 var GITHUB_TOKEN = "30300c552481a5a440ab1bd09a308617d45741e9";
 
 
+
 function getRepoContributors(repoOwner, repoName, callback) {
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 console.log(requestURL);
@@ -23,29 +24,36 @@ request.get(options, callback)
 
 }
 
+var urlList = []
+
 // calling my function getRepoContributors
 getRepoContributors("jquery", "jquery", function (error, response, body) {
     if (!error && response.statusCode == 200) {
-
       var data = JSON.parse(body)
-      console.log(data);
-    } else {
-      // didn't get an http response
-      console.log(error);
-      // got an https response, but not status == 200
-      console.log(response);
-    }
-  })
+      data.forEach (function (elm, index, array) {
+      urlList.push(elm["avatar_url"])
 
-  request.get('https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg')
+      })
+  }
+  console.log(urlList);
+})
+
+
+
+
+
+  request.get('https://avatars.githubusercontent.com/u/1615?v=3", "avatars/jeresig.jpg')
          // .on('error') handles any errors
          .on('error', function (err) {                                   // Note 2
            throw err;
          })
-         .pipe(fs.createWriteStream('./kvirani.jpg'));
+         .pipe(fs.createWriteStream('./jeresig.jpg'));
+
   //function that will make a request to a given url, saving the resulting
   // image file to a specified filePath.
+  //
+  function downloadImageByURL(url, filePath) {
 
-  // function downloadImageByURL(url, filePath) {
-  //   // ...
-  // }
+    // now I have my var urlList = [] to work with
+  //   request.get// forEach.data[avatar_url] // return image;
+  }
