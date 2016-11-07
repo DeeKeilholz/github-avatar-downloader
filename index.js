4307bedabc881b1be0fd249da8ed004eb559d0d4
DeeKeilholz <deekeilholz@gmail.com>
2016-11-07 22:29:11 +0000

    Add update to index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var request = require('request');
 // var token = require('./token');
 
-// var repos = JSON.pars(body);
-// console.log(repos)
+
+
 
 var GITHUB_USER = "DeeKeilholz";
 var GITHUB_TOKEN = "30300c552481a5a440ab1bd09a308617d45741e9";
@@ -12,16 +12,22 @@ function getRepoContributors(repoOwner, repoName, callback) {
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 console.log(requestURL);
 
-request.get('https://api.github.com/repos/jquery/jquery/contributors')
-
-       .on('response', function (response) {                           // Note 3
-         console.log('Response Status Code: ', response.statusCode);
-         console.log('Response Message:', response.statusMessage);
-       })
+var options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+// my get request
+request.get(options, callback)
 
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+
+
+
+// calling my function getRepoContributors
+getRepoContributors("jquery", "jquery", function (err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
-});
+   console.log("Result:", result);
+ });
