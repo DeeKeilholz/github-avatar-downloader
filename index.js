9fa71642f02ae3fd19ac0794cde809bdc0a6aa40
DeeKeilholz <deekeilholz@gmail.com>
2016-11-07 21:13:27 +0000

    updated js file

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 var request = require('request');
+// var token = require('./token');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// var repos = JSON.pars(body);
+// console.log(repos)
+
+var GITHUB_USER = "DeeKeilholz";
+var GITHUB_TOKEN = "30300c552481a5a440ab1bd09a308617d45741e9";
+
+
+function getRepoContributors(repoOwner, repoName, callback) {
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
+
+request.get('https://api.github.com/repos/jquery/jquery/contributors')
+
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Message:', response.statusMessage);
+       })
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
